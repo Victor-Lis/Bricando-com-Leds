7d5384a5767367ee8dd0cc3078c5b84461b93bdf
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-08 22:12:09 -0300

    v1.05
    - Criando função para simular um semáforo()

--- Pisca_Externo.ino
+++ Pisca_Externo.ino
@@ -59,6 +59,44 @@ void padrao3(){
   delay(tempo);             
 }
 
+void semaforo(){
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    //Serial.println("Led 3");
+  delay(tempo * 5);           
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    //Serial.println("Led 2");
+  delay(tempo);      
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);     
+    //Serial.println("    ");    
+  delay(tempo);      
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW); 
+    //Serial.println("Led 2");
+  delay(tempo);       
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);   
+    //Serial.println("    ");    
+  delay(tempo);
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);      
+    //Serial.println("Led 2");   
+  delay(tempo);      
+    digitalWrite(led1, HIGH);  
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);   
+    //Serial.println("Led 1");    
+  delay(tempo * 5);   
+}
+
 // the loop function runs over and over again forever
 void loop() {
   all();
