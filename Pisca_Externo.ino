f4b352b37cdf78f64b0d47b31c58e2d220a2d057
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-08 22:11:20 -0300

    v1.04
    - Criando função padrao3

--- Pisca_Externo.ino
+++ Pisca_Externo.ino
@@ -48,6 +48,17 @@ void padrao2(){
   delay(tempo);  
 }
 
+void padrao3(){
+    digitalWrite(led1, HIGH);  
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+  delay(tempo);           
+    digitalWrite(led1, LOW);  
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+  delay(tempo);             
+}
+
 // the loop function runs over and over again forever
 void loop() {
   all();
